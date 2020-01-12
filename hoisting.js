76c50f98cde5ca96d9ff74add3216b54a7be9895
Doris Le <dorisle@sandiego.edu>
2020-01-12 00:42:43 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,20 +1,21 @@
 function callMe() {
-  var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  return callMe(lyric);
+  return lyric;
+  var lyric = "maybe";
 }
-callMe();
+
 
 function crazy() {
-  var thisIsCrazy = function (){
-    console.log("hey!!!")
-  }// fix the code in here:
-}
+  // fix the code in here:
   thisIsCrazy();
 
+  var thisIsCrazy = function (){
+    console.log("hey!!!")
+  }
+}
 
 function sayMyName() {
   // fix the code in here:
